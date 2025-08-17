b516ffded00cdb89bffb06675b44e2e0dcd1c485
gabriellloc <gabrielocdev595@gmail.com>
2025-08-17 02:13:05 -0300

    Modal e Js
    Montei o modal e apliquei o evento de click no botão de cancelar.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const cancel = document.querySelector("#cancel")
+const modalAcolhimento = document.querySelector("#modal-acolhimento");
+
+cancel.addEventListener("click", ()=> {
+    modalAcolhimento.showModal()
+})
