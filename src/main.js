e2a7b81e2cb703993f5c1f808560adae5610be02
gabriellloc <gabrielocdev595@gmail.com>
2025-08-17 03:05:50 -0300

    adicionado ID para o Js
    Adicionando o Id para o Js e configurando o modal

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 "use strict";
 
-const cancel = document.querySelector("#cancel")
+const cancel = document.querySelector("#cancel");
 const modalAcolhimento = document.querySelector("#modal-acolhimento");
+const nav = document.querySelector("#nav");
+const content = document.querySelector("#content")
+
 
 cancel.addEventListener("click", ()=> {
-    modalAcolhimento.showModal()
+    modalAcolhimento.showModal();
+    nav.classList.add("blur")
+    content.classList.add("blur")
+})
+
+modalAcolhimento.addEventListener("close", (event)=> {
+    nav.classList.remove("blur")
+    content.classList.remove("blur")
 })
